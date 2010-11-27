167feed27aa7ca8499d67ed31ffb90451eebde6d
David Manura <dm.git@math2.org>
2010-11-26 19:02:06 -0500

    import

---
+++ README.txt
@@ -0,0 +1,92 @@
+LuaAnnotate - C code comments externally stored but displayed inline.
+
+This tool enables you to maintain C source file comments in a
+text file external to the source file itself and then later display
+those comments inline to the source.
+
+The comments are maintained in a text file (annotate.txt) in the same
+directory as the C sources.  Displaying the comments inline to the C
+code can be done in many ways:
+
+  - weaving the comments into the source code as C comments but saved
+    as a different file for viewing in any text editor.
+  - displaying annotations in the SciTE text editor when viewing the
+	  original source files (without altering those source files).
+  - other possibilities (not implemented) include rendering as HTML
+	   or creating plugins for other text editors.		
+
+The original motivation for this project is to allow Lua users to better
+document the Lua source code [2].  The Lua source code is dense
+but sparsely commented.  It's uncertain whether the original Lua authors
+will incorporate generous use of comments in the original upstream
+sources, so we need to maintain the comments separately.
+This becomes challenging when the upstream sources change
+and users themselves may maintain their own set of patches.
+The problem with using unified patches here is that the contextual
+information contained is more detailed than necessary and its conflict
+checking is more strict than necesessary.  All the comment data really
+needs to say is that a certain identifier corresponds with a certain piece of
+documentation, almost in the manner of ctags.  Furthermore, if such an
+identifier cannot be matched in the source, we don't care that much
+--it's just documentation.
+
+== Project Page ==
+
+For further details, see http://lua-users.org/wiki/LuaAnnotate .
+
+== Status ==
+
+WARNING: This code is very new and might not be stable yet.
+It is usable but you may need to sometimes fix things yourself.
+Many additional features could be added too.
+
+== Example ===
+
+See the "lua\annotations.txt" for an example of an annotations file
+for the Lua 5.2.0-alpha sources.  To use, copy annotations.txt into
+the Lua "src" directory.
+
+Note: This annotations.txt example currently only documents a few items
+in the Lua sources.  It should be extended to the extire Lua sources.
+Patches are welcome.
+
+== Command-line Usage (C++ output) ==
+
+Example:
+
+  $ cp lua/annotations.txt ../lua-5.2.0-alpha/src/
+  $ lua luaannotate.lua ../lua-5.2.0-alpha/src/luac.c > luac-annotated.c
+
+== Installation in SciTE ==
+
+First install SciTE <http://www.scintilla.org/SciTE.html>.
+Version 2.12 and 2.20 work (older versions might not work).
+
+Install SciTE ExtMan: http://lua-users.org/wiki/SciteExtMan .
+
+Copy luaannotate.lua into your ExtMan "scite_lua" scripts folder.
+
+Now when you restart SciTE and load a C file that has an annotations.txt
+file in the same directory, SciTE will render the annotations inline to the
+source file.
+
+== LICENSE ==
+
+See COPYRIGHT file.
+
+== Credits ==
+
+David Manura, original author.
+Steve Donovan originally suggested this.
+
+== Bugs ==
+
+Please report bugs via github <http://github.com/davidm/lua-annotate/issues>
+or just "dee em dot el you ae at em ae tee ayche two dot ow ar gee", or
+if you prefer neither then append to the wiki page
+<http://lua-users.org/wiki/LuaAnnotate>.
+
+== References ==
+
+[1] http://www.scintilla.org/SciTE.html
+[2] lua-users.org/wiki/LuaSource
---
+++ luaannotate.lua
@@ -0,0 +1,169 @@
+-- luaannotate.lua
+-- (C) 2010, David Manura
+
+
+-- Load annotations file.
+local function load_annotations(path)
+  local filename = path:match'[^\\/]+$'
+  if not filename then
+    error('Invalid path ' .. path)
+  end
+  
+  local annotations = {}
+  local id
+  local text = ''
+  local function commit()
+    if id then
+      --print('DEBUG-luacomment-commit', id, text)
+      text = text:gsub('[\r\n]$', '')
+      annotations[id] = text
+    end
+  end
+
+  -- Process line-by-line.
+  local fh = assert(io.open(path))
+  local linenum = 1
+  for line in fh:lines() do
+    if line:match'^#' then
+      commit()
+      local sfile, sname = line:match'^#%s*([^:]+):(%w*)'
+      if not sfile then
+        error('Invalid directive in ' .. path .. ':' .. linenum)
+      end
+      text = ''
+      id = sfile .. ':' .. sname
+    else
+      text = text .. (text ~= '' and '\n' or '') .. line
+    end
+    linenum = linenum + 1
+  end
+  commit()
+  
+  return annotations
+end
+
+-- Locates where annotations should go in source string `code`
+-- in file path `path`.
+local function place_annotations(code, path, annotations)
+  local sfile = path:match'[^\\/]+$'
+  
+  local notes = {}
+
+  -- Comments on functions/macros/structs
+  -- TODO: could be improved (robustness)
+  local iline = 1
+  for line in (code .. '\n'):gmatch'(.-)\r?\n' do
+    local sname =
+      line:match'^[%w ]-[%w*]+%s(%w+)%s*%b()%s*$' or
+      line:match'^#define%s+(%w+)' or
+      line:match'^typedef%s+struct%s+(%w+)' or
+      line:match'^typedef%s+union%s+(%w+)'
+    if sname then
+      local id = sfile .. ':' .. sname
+      local comment = annotations[id]
+      --print('DEBUG-luacomment-comment', comment, id)
+      if comment then
+        table.insert(notes, {iline, comment})
+      end
+    end
+    iline = iline + 1
+  end
+
+  -- Comments on file.
+  local filecomment = annotations[sfile .. ':']
+  if filecomment then
+    if notes[1] and notes[1][1] == 1 then -- existing comment on line 1
+      local comment = filecomment .. '\n\n' .. notes[1][2]
+      notes[1][2] = comment
+    else
+      table.insert(notes, 1, {1, filecomment})
+    end
+  end
+
+  return notes
+end
+
+-- Utility function to check whether file exists (actually, is readable).
+local function exists_file(path)
+  local fh = io.open(path)
+  if fh then fh:close() return true end
+  return false
+end
+
+-- Utility function to load file into string (text mode).
+local function read_file(path)
+  local fh, err = io.open(path)
+  if not fh then
+    error('Could not open ' .. path .. ' : ' .. err)
+  end
+  local data = fh:read'*a'
+  fh:close()
+  return data
+end
+
+-- SciTE specific
+if scite_OnOpen then -- if ExtMan installed
+  -- Add annotation notes to SciTE buffer.
+  local function scite_annotate_buffer(notes)
+    editor.AnnotationVisible = 2
+    for _, note in ipairs(notes) do
+      local iline, comment = unpack(note)
+      editor:AnnotationSetText(iline-1, comment)
+      --editor:AnnotationStyle[iline] = 1
+    end
+  end
+
+  -- SciTE ExtMan callback function, on file opened.
+  scite_OnOpen(function(path)
+    local annotations_path = path:gsub('[^\\/]+$', 'annotations.txt')
+    print('DEBUG-luacomment-open', path, annotations_path)
+    if exists_file(annotations_path) then
+      local code = editor:GetText()
+      local annotations = load_annotations(annotations_path)
+      local notes = place_annotations(code, path, annotations)
+      scite_annotate_buffer(notes)
+    end
+  end)
+	
+	return
+end
+
+-- Escapes characters in text to allow embedding inside C comment (/* */).
+local function cpp_escape_comment(text)
+  text = text:gsub('%*/', '* /')
+  return text
+end
+
+-- Adds annotations to code.
+local function cpp_annotate(code, notes)
+  local commentofline = {} -- index
+  for _, linecomment in ipairs(notes) do
+    local iline, comment = unpack(linecomment)
+    commentofline[iline] = comment
+  end
+
+  -- TODO: could be improved: avoid nested comments
+  local iline = 1
+  code = (code .. '\n'):gsub('(.-\r?\n)', function(line)
+    if commentofline[iline] then
+      line = '/*# ' .. cpp_escape_comment(commentofline[iline]) .. '*/\n' .. line
+    end
+    iline = iline + 1
+    return line
+  end)
+  return code
+end
+
+-- Command-line usage: lua luacomment.lua <filename>
+local path = ...
+if path then
+  local annotations_path = path:gsub('[^\\/]+$', 'annotations.txt')
+  local code = read_file(path)
+  local annotations = load_annotations(annotations_path)
+  local notes = place_annotations(code, path, annotations)
+  code = cpp_annotate(code, notes)
+  io.stdout:write(code)
+else
+  io.stderr:write('usage: luaannotate.lua <filename>')
+	os.exit(1)
+end
